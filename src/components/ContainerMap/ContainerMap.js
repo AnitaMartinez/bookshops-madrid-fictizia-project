d04b4265fa9580e080aeebb769adbcd6dc25a4cf
AnitaMartinez <amartinezaguilar6@gmail.com>
2020-05-03 18:37:50 +0200

    Replace google-map-react by leaflet

--- src/components/ContainerMap/ContainerMap.js
+++ src/components/ContainerMap/ContainerMap.js
@@ -1,32 +1,36 @@
 import React from 'react'
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import "./ContainerMap.css";
+import { Icon } from "leaflet";
+
 // import PropTypes from 'prop-types'
-import GoogleMapReact from 'google-map-react';
 
 const coordenates = {
-    center: {
-      lat: 40.4165001,
-      lng: -3.7025599
-    },
-    zoom: 11
-  };
+  lat: 40.417147,
+  lng: -3.703494,
+};
+
+const attributionOpenStreetMap = {
+  url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}
 
 
 const ContainerMap = (props) => {
+  const { lat, lng} = coordenates;
   return (
-    <div style={{ height: '50vh', width: '50%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: '' }}
-          defaultCenter={coordenates.center}
-          defaultZoom={coordenates.zoom}
-        >
-            <div
-                lat={40.417188}
-                lng={-3.703526}
-            >
-                <p>Soy la Puerta del Sol</p>
-            </div>
-        </GoogleMapReact>
-      </div>
+    <Map center={[lat, lng]} zoom={13}>
+      <TileLayer
+        url={attributionOpenStreetMap.url}
+        attribution={attributionOpenStreetMap.attribution}
+      />
+      <Marker
+          key={'id'}
+          position={[ lat, lng ]}
+          onClick={() => console.log('pulsado marker')}
+      >
+      </Marker>
+    </Map>
   )
 }
 
